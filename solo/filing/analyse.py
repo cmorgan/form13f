da487d656b971794d0fcab79715bf6dcc5b8ccb7
Chris Morgan <chris.j.morgan@gmail.com>
2013-05-29 19:30:06 +0100

    Anserw to Question 2.b)
    made some assumptions that 'largest' (2b) and 'biggest' (2c) refers to market
    value

--- solo/filing/analyse.py
+++ solo/filing/analyse.py
@@ -23,6 +23,7 @@ import datetime
 
 
 def get_common_stocks(data_frame):
+    print data_frame
     title_of_class = parse.COLUMN_NAMES[1]
     # filter TITLE OF CLASS = COM
     filter = data_frame[title_of_class] == 'COM'
@@ -49,8 +50,10 @@ def question_2_a(data_set):
 
     total_values = []
     for fname, conformed_period, filed_date, df in data_set:
-        total_values.append((fname, conformed_period, filed_date,
-                             total_market_value_of_COM(df)))
+        total_values.append(
+            (fname, conformed_period, filed_date,
+             total_market_value_of_COM(df))
+        )
 
     first_quarter_total = total_values[0][3]
     last_quarter_total = total_values[-1][3]
@@ -71,7 +74,7 @@ def get_new_stocks(data_frame1, data_frame2):
     return data_frame1.ix[new_stocks]
 
 
-def get_top_holdings(data_frame, n):
+def get_top_market_value(data_frame, n):
     "return top n holdings from a data_frame"
 
     # sort by market value
@@ -79,17 +82,33 @@ def get_top_holdings(data_frame, n):
     return df_by_market_value[0: n]
 
 
+def get_closest_data(data_set, target_date):
+    """given set of data return that which has closest conformed period to
+    target date"""
+
+    date_diffs = []
+
+    for i, data in enumerate(data_set):
+        report_date = data[1]
+        # report_date must be greater than target to be of interest
+        if report_date > target_date:
+            date_diffs.append((i, report_date - target_date))
+
+    # sort by date diff ascending
+    sorted_diffs = sorted(date_diffs, key=lambda x: x[1])
+
+    return data_set[sorted_diffs[0][0]]
+
+
 def question_2_b(data_set):
     """b) What would have been the 5 largest holdings of common stock that were
     publically available on 12 August 2012 for the fund manager?"""
-    pass
+    target_date = datetime.datetime(2012, 8, 12)
 
-    #target_date = datetime.datetime(2012, 8, 12)
-    #print target_date
-    #for fname, conformed_period, filed_date, df in data_set:
-    #    if conformed_period
-    
-    # get correct dataset for target date
+    _, _, _, data_frame = get_closest_data(data_set, target_date)
+
+    # assuming 'largest holdings' means by market value not no. of shares
+    return get_top_market_value(data_frame, 5)
 
 
 def question_2_c(data_set):
@@ -104,12 +123,13 @@ def question_2_c(data_set):
     # check the last conformed data equals target
     assert sorted_data_set[-1][1] == target_conformed_date, 'Got wrong date'
 
-    target_data_frame = sorted_data_set[-1]
-    previous_data_frame = sorted_data_set[-2]
+    target_data_frame = sorted_data_set[-1][3]
+    previous_data_frame = sorted_data_set[-2][3]
 
     new_stocks = get_new_stocks(target_data_frame, previous_data_frame)
 
-    top_holdings = get_top_holdings(new_stocks, 3)
+    # assuming 'biggest new common stock...' means by market value
+    top_holdings = get_top_market_value(new_stocks, 3)
 
     return top_holdings
 
@@ -120,5 +140,5 @@ def analyse_all():
     assert len(quarters) == 4, 'Wrong number of quarters'
 
     print question_2_a(quarters)
+    print question_2_b(quarters)
     print question_2_c(quarters)
-    #return question_2_a(quarters)
